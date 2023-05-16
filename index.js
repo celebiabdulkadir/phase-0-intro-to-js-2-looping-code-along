75d67bd271afba21c1133dadfeeef363599c5f2a
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2023-05-16 13:07:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const writeCards = (array) => {
+	const newArray = [];
+	for (let i = 0; i < array.length; i++) {
+		newArray.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+	}
+	return newArray;
+};
+
+const countDown = (count) => {
+	for (let i = count; i >= 0; i--) {
+		console.log(i);
+	}
+};
